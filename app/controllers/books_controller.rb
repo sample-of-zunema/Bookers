3a27b4025ea6b4a4d5e22044bd5e552091a573c1
zunema <yxxa.abc@gmail.com>
2020-11-05 08:17:06 +0000

    [update]createアクションを追加して投稿機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def top
   end
 
@@ -9,8 +10,21 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
