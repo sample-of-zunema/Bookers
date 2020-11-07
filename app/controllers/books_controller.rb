a40872437360e400ab3bd9cb2732efea041c0e16
zunema <yxxa.abc@gmail.com>
2020-11-07 23:44:13 +0000

    [update]index/show/edit/controllerの編集/pathの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @book = Book.all
+    @books = Book.all
   end
 
   def show
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
 
   private
