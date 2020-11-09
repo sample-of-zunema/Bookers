3783b1a7017d39e69e5306c7f830197398d1320c
zunema <yxxa.abc@gmail.com>
2020-11-09 09:16:57 +0000

    [update]rspec導入とエラー解決

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-    validates :title, presence: {message: 'error' }
-    validates :body,presence: {message: 'error' }
+    validates :title, presence: true
+    validates :body, presence: true
 end
